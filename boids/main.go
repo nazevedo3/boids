884c4cc77283d68481079d411dc442a72506d781
Nathan Azevedo <nathan@Nathans-MacBook-Pro.local>
2021-05-12 12:24:37 -0700

    added mutex's

--- boids/main.go
+++ boids/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"image/color"
 	"log"
+	"sync"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
@@ -10,11 +11,15 @@ import (
 const (
 	screenWidth, screenHeight = 640, 360
 	boidCount                 = 500
+	viewRadius                = 13
+	adjRate                   = 0.015
 )
 
 var (
-	green = color.RGBA{10, 255, 50, 255}
-	boids [boidCount]*Boid
+	green   = color.RGBA{10, 255, 50, 255}
+	boids   [boidCount]*Boid
+	boidMap [screenWidth + 1][screenHeight + 1]int
+	rWlock  = sync.RWMutex{}
 )
 
 type Game struct{}
@@ -37,6 +42,11 @@ func (g *Game) Layout(_, _ int) (w, h int) {
 }
 
 func main() {
+	for i, row := range boidMap {
+		for j := range row {
+			boidMap[i][j] = -1
+		}
+	}
 	for i := 0; i < boidCount; i++ {
 		createBoid(i)
 	}
